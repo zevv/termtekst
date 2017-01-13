56d6ab6997d510906df21b405bff57478e29c2cb
Willem Jan Faber <willemjan.faber@fe2.nl>
2017-01-13 20:42:42 +0100

    added tt.py

---
+++ tt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+import json
+import ncurses
+import requests
+
+
+def main():
+    page = 100
+    if int(sys.argv[1]) and int(sys.argv[1]) < 990:
+        page = int(sys.argv[1])
+    else:
+        
+
+
+    
+
+if __name__ == '__main__':
+    main()
