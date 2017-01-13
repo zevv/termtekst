61ed9423799a9c72bbcf2d8985bdac02d0b9ec7e
WillemJan <FaberWillemJan@gmail.com>
2017-01-13 21:50:29 +0100

    added setup.py

--- tt.py
+++ tt.py
@@ -3,20 +3,53 @@
 import os
 import sys
 
+from lxml import html
 import json
-import ncurses
-import requests
 
+TELETEKST = "http://teletekst-data.nos.nl/json/%i"
+
+error = ['Unable to import... \n %s\n',
+        '']
+
+warn = ['Defaulting to page 100 \n',
+        '']
 
 def main():
+    '''
+    >>> main()
+    http://teletekst-data.nos.nl/json/100
+    '''
+
+    try:
+        import ncurses
+    except ImportError as e:
+        msg = error[0] % str(e)
+        sys.stderr.write(msg)
+        sys.exit(-1)
+        pass
+
+    try:
+        import requests
+    except ImportError as e:
+        msg = error[0] % str(e)
+        sys.stderr.write(msg)
+        sys.exit(-1)
+        pass
+
     page = 100
-    if int(sys.argv[1]) and int(sys.argv[1]) < 990:
-        page = int(sys.argv[1])
-    else:
-        
+    try:
+        if int(sys.argv[1]) and int(sys.argv[1]) < 999:
+            page = int(sys.argv[1])
+    except:
+        msg = warn[0] + '\n'
+        sys.stdout.write(msg)
 
-
-    
+    page = TELETEKST % page
+    print(page)
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        import doctest
+        doctest.testmod()
+    else:
+        main()
