0c99d1890f398c733df5cde6c655be798ea8f99b
GijsTimmers <timmers.gijs@gmail.com>
2017-06-10 11:31:24 +0200

    setup.py file for easy installing
    please install using
    
    sudo ./setup.py install

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup, find_packages
+import sys
+import os
+
+
+def check_requirements():
+    assert os.geteuid() == 0, "Please run with root privileges."
+
+try:
+    check_requirements()
+    setup(
+    name = "termtekst",
+    packages = find_packages(),
+    version = "1.0.0",
+    description = "NOS teletekst on the linux console",
+    author = "Ico Doornekamp",
+    author_email = "github@zevv.nl",
+    url = "https://github.com/zevv/termtekst",
+    keywords = ["teletekst", "terminal"],
+    install_requires = [],
+    classifiers = [],
+    scripts=["src/tt"],
+    include_package_data = True
+    )
+except AssertionError as e:
+    print(e)
